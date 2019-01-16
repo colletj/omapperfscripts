9363539290947c8c2b902037efd9cd0dddb58cd8
Julien Collet <julien.collet@cern.ch>
2019-01-16 17:50:34 +0100

    drafting aio ops

--- omap-test.py
+++ omap-test.py
@@ -3,7 +3,7 @@
 # julien.collet@cern.ch 
 # omap benchmarks, 2019
 
-import rados, sys, time
+import rados, sys, time, threading
 
 print("Running omap benchmarking tests");
 
@@ -50,7 +50,11 @@ print "\nListing objects in the pool"
 print "------------------"
 
 
-n=10000
+n=1000
+
+
+print "\nBenchmarking synchronous omap ops"
+print "------------------"
 
 st = time.time()
 with rados.WriteOpCtx(ioctx) as wop:
@@ -67,8 +71,50 @@ with rados.ReadOpCtx(ioctx) as rop:
 
 rd = time.time();
 
-print "set " + str(n) + " omap pairs in " + str(wr - st) + "s"
-print "got " + str(n) + " omap pairs in " + str(rd - wr) + "s"
+print "set (sync) " + str(n) + " omap pairs in " + str(wr - st) + "s"
+print "got (sync) " + str(n) + " omap pairs in " + str(rd - wr) + "s"
+
+
+
+#print "\nBenchmarking asynchronous omap ops"
+#print "------------------"
+#
+#
+#lock = threading.Condition();
+#count = [0];
+#def cb(blah):
+#    with lock:
+#        count[0] += 1;
+#        lock.notify();
+#    return 0;
+#
+#st = time.time()
+#with rados.WriteOpCtx(ioctx) as wop:
+#    for i in range(0,n):
+#        ioctx.set_omap(wop, (str(i),), ("val"+str(i),));
+#        comp = ioctx.operate_aio_write_op(wop, "test", cb, cb);
+#        comp.wait_for_complete();
+#        comp.wait_for_safe();
+#        with lock:
+#            while count[0] < 10:
+#                lock.wait();
+#        comp.get_return_value()
+#        
+#
+#wr = time.time();
+#
+##with rados.ReadOpCtx(ioctx) as rop:
+##    iter, r = ioctx.get_omap_vals(rop, "", "", n);
+##    ioctx.operate_read_op(rop, "test");
+#
+#rd = time.time();
+#
+#print "set (aio) " + str(n) + " omap pairs in " + str(wr - st) + "s"
+#print "got (aio) " + str(n) + " omap pairs in " + str(rd - wr) + "s"
+
+
+
+
 
 print "\nClosing the connection."
 ioctx.close();
